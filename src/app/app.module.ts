74f47da31b57bf1f4d6e1e4d3bbfc4a3f7cdd71a
Ikechukwu Ekwe <iekwe@verifynigeria.com>
2020-01-14 10:33:33 +0100

    initial commit file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MaintenanceListComponent } from './maintenance-list/maintenance-list.component';
+import { MaintenanceCreateComponent } from './maintenance-create/maintenance-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MaintenanceListComponent,
+    MaintenanceCreateComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/maintenance-create/maintenance-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maintenance-create',
+  templateUrl: './maintenance-create.component.html',
+  styleUrls: ['./maintenance-create.component.scss']
+})
+export class MaintenanceCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
